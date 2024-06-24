58310f7b3151fafa33d1224d516c93bdefe13d4e
Denis Larionov <denlarionov@gmail.com>
2024-06-24 17:41:08 +0300

    d.larionov lesson_3

---
+++ homework/denis_larionov/lesson3/Task1.py
@@ -0,0 +1,8 @@
+a = 2
+b = 3
+summa = a + b
+delta = a - b
+multiplication = a * b
+print(summa)
+print(delta)
+print(multiplication)
---
+++ homework/denis_larionov/lesson3/Task3.py
@@ -0,0 +1,6 @@
+a = 154
+b = 555
+average = (a + b) / 2
+gmean = (a * b) ** 0.5
+print(average)
+print(gmean)
