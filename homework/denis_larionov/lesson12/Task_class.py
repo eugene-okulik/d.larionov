e1d885bf2c9f2327cdf93a1977279ddf556432c9
Denis Larionov <denlarionov@gmail.com>
2024-07-13 23:35:43 +0300

    d.larionov lesson12

--- homework/denis_larionov/lesson12/Task_class.py
+++ homework/denis_larionov/lesson12/Task_class.py
@@ -68,8 +68,8 @@ class Bouquet:
 
     def price_bouquet(self):
         print(flower_rose.price * flower_rose.count
-            + flower_lily.price * flower_lily.count
-            + flower_orchid.price * flower_orchid.count)
+                + flower_lily.price * flower_lily.count
+                + flower_orchid.price * flower_orchid.count)
 
     def life_bouquet(self):
         lf_b = (flower_rose.avg_lf_flowers + flower_lily.avg_lf_flowers + flower_orchid.avg_lf_flowers) / 3
@@ -78,19 +78,19 @@ class Bouquet:
     def sortet_flowers(self):
         while True:
             question = int(input(
-                            "'Введите значение сортировки цетов в букете:\n"
-                            " 1 - сортировка по цвету\n"
-                            " 2 - сортировка по длине стебля\n"
-                            " 3 - сортировка по стоимости'\n"))
+                        "'Введите значение сортировки цетов в букете:\n"
+                        " 1 - сортировка по цвету\n"
+                        " 2 - сортировка по длине стебля\n"
+                        " 3 - сортировка по стоимости'\n"))
             if question == int(1):
                 print(flower_rose.colour, bouquet.count_rose, flower_lily.colour,
-                       bouquet.count_lyli, flower_orchid.colour, bouquet.count_orchid)
+                    bouquet.count_lyli, flower_orchid.colour, bouquet.count_orchid)
             elif question == int(2):
                 print(flower_rose.type_flower, flower_rose.long_steam, flower_lily.type_flower,
-                      flower_lily.long_steam, flower_orchid.type_flower, flower_orchid.long_steam)
+                    flower_lily.long_steam, flower_orchid.type_flower, flower_orchid.long_steam)
             elif question == int(3):
                 print(flower_rose.type_flower, flower_rose.price, flower_lily.type_flower,
-                      flower_lily.price, flower_orchid.type_flower, flower_orchid.price)
+                    flower_lily.price, flower_orchid.type_flower, flower_orchid.price)
             break
 
     def search_flower(self):
