0135cf26ce5cd8a5e56b786f38ec5df7152e14a0
Denis Larionov <denlarionov@gmail.com>
2024-07-24 12:05:31 +0300

    d.larionov lesson14

--- homework/denis_larionov/lesson14/Script_SQL.sql
+++ homework/denis_larionov/lesson14/Script_SQL.sql
@@ -2,7 +2,7 @@ INSERT into `groups` (title, start_date, end_date) values ('Qiwi', 'june 2010', 
 select * from `groups` g where id = 1550;
 INSERT into students (name, second_name, group_id) values ('Qiwi', 'Pticsa', 1550);
 select * from students WHERE name LIKE 'Qi%';
-SELECT * from books where taken_by_student_id = 1620;
+SELECT * from books where taken_by_student_id = 1629;
 INSERT INTO books (title, taken_by_student_id) values
 ('Qiwi_lol', 1620),
 ('Qiwi_work', 1620),
@@ -18,18 +18,25 @@ values
 ('Lessons1_SQL', 2070),
 ('Lessons2_SQL', 2070);
 select id from students WHERE name = 'Qiwi';
-SELECT * from marks where student_id = 1620;
+SELECT * from marks where student_id = 1629;
 INSERT INTO marks (value, lesson_id, student_id) values 
 (5, 4624, (select id from students WHERE name = 'Qiwi')),
 (5, 4625, (select id from students WHERE name = 'Qiwi')),
 (5, 4626, (select id from students WHERE name = 'Qiwi'));
 
 
-select value from marks where student_id = 1620;
-SELECT title from books WHERE taken_by_student_id = 1620;
+select value from marks where student_id = 1629;
+SELECT title from books WHERE taken_by_student_id = 1629;
 SELECT * from `groups` g join students s on
 (g.id = s.group_id) join books b on
 (b.taken_by_student_id = s.id) join marks m on
 (s.id = m.student_id) join lessons l on
 (m.lesson_id = l.id) join subjets s2 on
-(s2.id = l.subject_id) where s.id = 1620;
+(s2.id = l.subject_id) where s.id = 1629;
+
+INSERT INTO students (name, second_name) values ('Qiwi2', 'Pticsa2');
+SELECT * from students s where name = 'Qiwi2';
+update students set group_id = 1550 where name = 'Qiwi2';
+DELETE from books where id in (3106, 3105, 3104);
+UPDATE books set taken_by_student_id = 1629 where taken_by_student_id = 1620;
+UPDATE marks set student_id = 1629 where student_id = 1620
